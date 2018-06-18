d3b4a56ab055c7b0615959472f2c32ea309186fc
TYSS <TYSS@TYSS-TR-009>
2018-06-18 16:40:44 +0530

    commit

--- flipkart/src/test/java/com/flipkart/test/test.java
+++ flipkart/src/test/java/com/flipkart/test/test.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class test {
 @Test
 public void testMethod(){
-	System.out.println("Maven executed");
+	System.out.println("Maven executed11");
+	System.out.println("Maven executed22");
 }
 }
